26ac52e2682faafe28a8f8ee2023b488511359a2
Agostinho Lucas Deus <agostinholucasdeus@gmail.com>
2022-05-28 22:36:10 -0300

    2/2022 - Refatorando métodos, criando métodos static para configurar startup.

---
+++ StoreEnterprise/src/services/SE.Identidade.API/Configuration/IdentityConfig.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using SE.Identidade.API.Data;
+using SE.Identidade.API.Extensions;
+using SE.Identidade.API.Models;
+using System.Text;
+
+namespace SE.Identidade.API.Configuration
+{
+    public static class IdentityConfig
+    {
+        public static IServiceCollection AddIdentityConfiguration(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(optionsAction: options =>
+           options.UseNpgsql(configuration.GetConnectionString(name: "StoredEnterprise")));
+
+            services.AddDefaultIdentity<IdentityUser>()
+                .AddRoles<IdentityRole>()
+                .AddErrorDescriber<IdentityMensagensPortugues>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
+            //JWT
+            var appSettingSection = configuration.GetSection(key: "AppSettings");
+            services.Configure<AppSettings>(appSettingSection);
+
+            var appSettings = appSettingSection.Get<AppSettings>();
+            var key = Encoding.ASCII.GetBytes(appSettings.Secret);
+
+            services.AddAuthentication(configureOptions: options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(bearerOptions =>
+            {
+                bearerOptions.RequireHttpsMetadata = true;
+                bearerOptions.SaveToken = true;
+                bearerOptions.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidAudience = appSettings.ValidoEm,
+                    ValidIssuer = appSettings.Emissor
+                };
+            });
+
+            return services;
+        }
+
+        public static IApplicationBuilder UseAddIdentityConfiguration(this IApplicationBuilder app)
+        {
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            return app;
+        }
+    }
+}
---
+++ StoreEnterprise/src/services/SE.Identidade.API/Configuration/SwaggerConfig.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+using System;
+
+namespace SE.Identidade.API.Configuration
+{
+    public static class SwaggerConfig
+    {
+        public static IServiceCollection AddSwaggerConfiguration(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc(name: "v1", new OpenApiInfo
+                {
+                    Title = "Store Enterprise Identity API",
+                    Description = "Esta API faz parte do projeto Store Enterprise",
+                    Contact = new OpenApiContact() { Name = "Agostinho Lucas Deus", Email = "agostinholucasdeus@gmail.com" },
+                    License = new OpenApiLicense() { Name = "LINKEDIN", Url = new Uri("https://linkedin.com/in/agostinho-lucas-deus") }
+                });
+            });
+
+            return services;
+        }
+
+        public static IApplicationBuilder USeSwaggerConfiguration(this IApplicationBuilder app)
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Store.Enterprise.Api v1"));
+            });
+
+            return app;
+        }
+    }
+}
