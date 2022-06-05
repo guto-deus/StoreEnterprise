57e47dac30fba4bc08802af07e266a4665a5662f
Agostinho Lucas Deus <agostinholucasdeus@gmail.com>
2022-06-05 11:47:41 -0300

    4/2022 - Apagando o projeto razor page e criando mvc tradicional.

---
+++ StoreEnterprise/src/web/SE.WebApp.MVC/Services/IAutenticacaoService.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+using static SE.WebApp.MVC.Models.UsuarioViewModel;
+
+namespace SE.WebApp.MVC.Services
+{
+    public interface IAutenticacaoService
+    {
+        Task<string> Login(UsuarioLogin usuarioLogin);
+        Task<string> Registro(UsuarioRegistro usuarioRegistro);
+    }
+}
